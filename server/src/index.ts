60502daa6b8c2d99b4e058527ced463fb04561e8
unknown <phillip@lunenetworks.com>
2023-07-31 22:37:43 -0700

    Created server directory and example express file

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app: express.Application = express();
+const port: number = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
